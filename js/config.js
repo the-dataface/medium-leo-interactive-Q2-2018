ec6fd1957c227c8956e53ada49f5bd79194a109d
Michael Hester <michaelvhester@gmail.com>
2018-05-31 18:47:09 -0400

    first commit

---
+++ js/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key : 'whGrkweiTUUbksYkC1YoT7Epj',
+    consumer_secret : '	PKcyKgTToeMZokfkzVrQEJwSjsPW5Oq54iGMVB2K8bRjW5iBrW',
+    token : '935276554212356102-PVGYu4NQ6sLndocqBIUIsUdc4rPySto',
+    token_secret : 'yT7gwj32FwdOjjjYv8MVFEuBSQLMyBIGkV7xezfE4WHxI'
+}
