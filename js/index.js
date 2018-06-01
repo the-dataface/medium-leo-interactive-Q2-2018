b9b37f12e8e6fc55a530b33096fccfdc0ac05ec6
Michael Hester <michaelvhester@gmail.com>
2018-05-31 20:00:42 -0400

    update card height

--- js/index.js
+++ js/index.js
@@ -187,11 +187,9 @@ map.on('style.load', () => {
 		
 		if (largeScreen || mediumScreen) {
 			if (largeScreen) {
-				card.style('top', 40 + 'px')
-					.style('max-height', window.innerHeight - 40 + 'px');
+				card.style('top', 40 + 'px');
 			} else {
-				card.style('top', 40 + 'px')
-					.style('max-height', window.innerHeight - 40 - 70 + 'px');
+				card.style('top', 40 + 'px');
 			}
 			card.style('width', (window.innerWidth - headerWidth) / 2 + 'px')
 				
